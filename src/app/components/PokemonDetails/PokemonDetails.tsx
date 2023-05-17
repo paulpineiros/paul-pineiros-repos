497206b63b4a285bb964e50ed0a2c74ed85cd492
Paul Pineiros <paulpieiros@gmail.com>
2023-05-17 14:34:19 -0500

    add types sprites weight

--- src/app/components/PokemonDetails/PokemonDetails.tsx
+++ src/app/components/PokemonDetails/PokemonDetails.tsx
@@ -30,9 +30,26 @@ const PokemonDetails = (props: Props) =>{
         <div className='contentDetail'>
             <div>
             <CardPokemon pokemon={selectedPokemon}/>
-            <div className='info'>
+            <div className='infoContent'>
+                <div className='title'>Tipos</div>
+                {selectedPokemon.types.map(type => (<span className='desc'>{type} </span>))}
+            </div>
+            <div className='infoContent'>
                 <div className='title'>Peso</div>
                 <div className='desc'>{selectedPokemon.weight}</div>
+            </div>
+            <div className='infoContent'>
+                <div className='title'>Sprites</div>
+                {selectedPokemon.sprites.slice(0, 10).map(sprite => (<span className='imgSprite'>
+                <img
+                    src={sprite}
+                    alt={`Imagen ${sprite}`}
+                    height={60}
+                /> </span>))}
+            </div>
+            <div className='infoContent'>
+                <div className='title'>Movimientos</div>
+                {selectedPokemon.moves.slice(0, 10).map(move => (<span className='desc'>{move} </span>))}
             </div>
             </div>
             
